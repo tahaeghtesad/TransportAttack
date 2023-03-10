0d24afbe33319ecb32b8a7f46258fa23b6237b01
tahaeghtesad <tahaeghtesad@gmail.com>
2023-03-10 13:45:47 -0500

    fix a typo in runner.py

--- runner.py
+++ runner.py
@@ -3,4 +3,4 @@ import subprocess
 if __name__ == '__main__':
     for epsilon in [15, 30]:
         for norm in [1, 2]:
-            subprocess.Popen(['python', '--norm', str(norm), '--epsilon', str(epsilon)])
+            subprocess.Popen(['python', 'mp_ddpg.py', '--norm', str(norm), '--epsilon', str(epsilon)])
