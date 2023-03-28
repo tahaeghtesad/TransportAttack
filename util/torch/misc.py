daf47794ac95ecd9a0cc663d4f2f519f4a63e3ee
tahaeghtesad <tahaeghtesad@gmail.com>
2023-03-28 14:59:09 -0400

    fix cuda backend

--- util/torch/misc.py
+++ util/torch/misc.py
@@ -4,7 +4,7 @@ import torch
 def allocate_best_device():
     if torch.backends.mps.is_available():
         return torch.device('mps')
-    elif torch.backends.cuda.is_available():
+    elif torch.cuda.is_available():
         return torch.device('cuda')
     else:
         return torch.device('cpu')
