a5516f72dabdc0e254ce891c2a050ca450cfa735
tahaeghtesad <tahaeghtesad@gmail.com>
2024-01-05 15:06:59 -0500

    add a noop detector

---
+++ models/heuristics/detectors.py
@@ -0,0 +1,15 @@
+import torch
+
+from models.dl.detectors import BaseDetector
+
+
+class ZeroDetector(BaseDetector):
+
+    def __init__(self):
+        super().__init__('ZeroDetector')
+
+    def forward(self, edge_travel_times, deterministic):
+        return torch.unsqueeze(torch.zeros(edge_travel_times.shape[0], device=edge_travel_times.device), dim=1)
+
+    def _update(self, edge_travel_times, decisions, next_edge_travel_times, rewards, dones):
+        pass
