49b0d0a54732051039f3b6ba7acc3c5ad43c6620
tahaeghtesad <tahaeghtesad@gmail.com>
2024-01-05 14:02:37 -0500

    fixed the __device property of CustomModule

--- models/dl/util.py
+++ models/dl/util.py
@@ -8,8 +8,8 @@ class CustomModule(torch.nn.Module, ABC):
     def __init__(self, name):
         super().__init__()
         self.name = name
-        self.logging = logging.getLogger(name)
-        self.__device = None
+        self.logger = logging.getLogger(name)
+        self.__device: torch.device = None
 
     def _get_name(self):
         return f'{self.name}'
@@ -19,22 +19,16 @@ class CustomModule(torch.nn.Module, ABC):
 
     @property
     def device(self) -> torch.device:
+        if self.__device is not None:
+            return self.__device
         for param in self.parameters():
             return param.device
         return torch.device('cpu')
 
-    # @property
-    # def device(self) -> torch.device:
-    #     if self.__device is not None:
-    #         return self.__device
-    #     for param in self.parameters():
-    #         return param.device
-    #     return torch.device('cpu')
-    #
-    # def to(self, *args, **kwargs):
-    #     self.__device = args[0]
-    #     for module in self.modules():
-    #         if isinstance(module, CustomModule):
-    #             if module.__device is None:
-    #                 module.to(*args, **kwargs)
-    #     return super().to(*args, **kwargs)
+    def to(self, *args, **kwargs):
+        self.__device = args[0]
+        for module in self.modules():
+            if isinstance(module, CustomModule):
+                if module.__device is None:
+                    module.to(*args, **kwargs)
+        return super().to(*args, **kwargs)
